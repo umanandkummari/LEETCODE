87517f1705ce2e712214ac59fb2c589a0b1c776c
umanandkummari <umanandkummari2113@gmail.com>
2023-02-24 13:40:47 +0530

    live class 1

--- src/Arrays/TwoSum.java
+++ src/Arrays/TwoSum.java
@@ -1,7 +1,9 @@
 package Arrays;
-
 public class TwoSum {
     public static void main(String[] args) {
         System.out.println("hello world");
+    }
+    public static void helloWOrld(int a){
+        System.out.println("hlo");
     }
 }
